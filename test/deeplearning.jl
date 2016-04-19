10307219e6a552b74a25ee54327d17ed6be4ecfb
CarloLucibello <carlo.lucibello@gmail.com>
2016-04-19 15:59:59 +0200

    add forward

--- test/deeplearning.jl
+++ test/deeplearning.jl
@@ -1,30 +1,30 @@
-# ### PERCEPTRON
-# @time g, W, E, stab = DeepBinary.solve(α=0.5, K=[201,1]
-#             , layers=[:ms]
-#             ,r=1.,r_step=0.0, seedξ=1,maxiters=1000);
-# @test E == 0
-#
-# for lay in [:tapex,:bpex]
-#     @time g, W, E, stab = DeepBinary.solve(α=0.7, K=[101,1]
-#                 , layers=[lay]
-#                 ,r=.3,r_step=0.002, seedξ=1,maxiters=500);
-#     @test E == 0
-# end
-#
-# for lay in [:tap,:bp]
-#     @time g, W, E, stab = DeepBinary.solve(α=0.7, K=[1001,1]
-#                 , layers=[lay]
-#                 ,r=.6,r_step=0.05, seedξ=1,maxiters=500);
-#     @test E == 0
-# end
-#
-# #### COMMITTEE
-# for lay in [:tapex, :bpex]
-#     @time g, W, E, stab = DeepBinary.solve(α=0.2, K=[1001,7,1]
-#                 , layers=[:tap,lay]
-#                 ,r=.8,r_step=0.01, seedξ=1,maxiters=500);
-#     @test E == 0
-# end
+### PERCEPTRON
+@time g, W, E, stab = DeepBinary.solve(α=0.5, K=[201,1]
+            , layers=[:ms]
+            ,r=1.,r_step=0.0, seedξ=1,maxiters=1000);
+@test E == 0
+
+for lay in [:tapex,:bpex]
+    @time g, W, E, stab = DeepBinary.solve(α=0.7, K=[101,1]
+                , layers=[lay]
+                ,r=.3,r_step=0.002, seedξ=1,maxiters=500);
+    @test E == 0
+end
+
+for lay in [:tap,:bp]
+    @time g, W, E, stab = DeepBinary.solve(α=0.7, K=[1001,1]
+                , layers=[lay]
+                ,r=.6,r_step=0.05, seedξ=1,maxiters=500);
+    @test E == 0
+end
+
+#### COMMITTEE
+for lay in [:tapex, :bpex]
+    @time g, W, E, stab = DeepBinary.solve(α=0.2, K=[1001,7,1]
+                , layers=[:tap,lay]
+                ,r=.8,r_step=0.01, seedξ=1,maxiters=500);
+    @test E == 0
+end
 
 ### 3 LAYERS
 @time g, W, E, stab = DeepBinary.solve(α=0.2, K=[401,21,3,1]
