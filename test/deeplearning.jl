29469650abc2f0a2f794c5895e3e60b7289cc215
CarloLucibello <carlo.lucibello@gmail.com>
2016-05-03 20:33:44 +0200

    metto il comando per il terzo livello nei test

--- test/deeplearning.jl
+++ test/deeplearning.jl
@@ -49,4 +49,9 @@ end
 ####             ,r=.95,rstep=0.005, seedξ=1,maxiters=1000);
 ####
 
+# IMPARATO TERZO LIVELLO!!!!
+# @time g, W, E, stab = DeepBinary.solve(α=0.15, K=[301,21,11,3,1]
+#                 , layers=[:tap,:tap,:tapex,:bpex]
+#                 ,r=.9, rstep=0.0005, ry=0.01, seedξ=1, maxiters=2000);
+
 ##########################
