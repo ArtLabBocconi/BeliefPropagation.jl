65662737b0d1d832856ea053febe3fcf271730ca
Carlo Lucibello <carlo.lucibello@gmail.com>
2021-04-04 09:38:25 +0200

    slight change dis ferr

--- examples/disordered_ferromagnet_2d.jl
+++ examples/disordered_ferromagnet_2d.jl
@@ -6,15 +6,15 @@ using Random, Statistics
 
 # 2D lattice
 
-T = 1
+T = 2
 L = 8
 net = Grid([L, L], Network, periodic=true)
 # Set J and H has graph properties since they are constants
 gprop!(net, "T", T)
 eprop!(net, "J", e -> rand()) 
-vprop!(net, "H", v -> rand())
+vprop!(net, "H", v -> randn())
 
-fg = Ising.run_bp(net, T=T, maxiters=1000, μ=1, verbose=false);
+fg = Ising.run_bp(net, T=T, maxiters=1000, μ=1, verbose=true);
 
 vprop!(net, "bp_mags", fg.mags)
 eprop!(net, "bp_umess", e -> Ising.getmess(fg, src(e), dst(e)))
